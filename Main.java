7bfa2c652ef04a45a8bb30690fa1b72e65245043
Mourad <mourad1081@gmail.com>
2016-11-07 19:20:43 +0100

    add hello world test

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     Graph g = new Graph();
     // Add an edge to the graph
-
+    System.out.println("hello world !");
     g.addRoot(new Vertex());
   }
 }
