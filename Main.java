5356c6639d2bb6ade546a4008b4c35ef67b36f8e
Mourad <mourad1081@gmail.com>
2016-11-07 21:00:11 +0100

    Added javadoc and some methods

--- Main.java
+++ Main.java
@@ -1,10 +1,58 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        // graph of the figure 1 of the project file
+        // Graph L_M_N
 
-  public static void main(String[] args) {
-    Graph g = new Graph();
-    // Add an edge to the graph
-    System.out.println("hello world !");
-    g.addRoot(new Vertex());
-  }
+        Vertex<String> M = new Vertex<>("M");
+        Vertex<String> N = new Vertex<>("N");
+        Vertex<String> L = new Vertex<>("L");
+        Edge MN = new Edge(M, N, 10);
+        Edge LM = new Edge(L, M, 20);
+        Edge LN = new Edge(L, N, 60);
+        M.addEdge(MN);
+        L.addEdge(LM);
+        L.addEdge(LN);
+
+        // Graph A_B_C_D_E_F_G_H
+        Vertex<String> A = new Vertex<>("A");
+        Vertex<String> B = new Vertex<>("B");
+        Vertex<String> C = new Vertex<>("C");
+        Vertex<String> D = new Vertex<>("D");
+        Vertex<String> E = new Vertex<>("E");
+        Vertex<String> F = new Vertex<>("F");
+        Vertex<String> G = new Vertex<>("G");
+        Vertex<String> H = new Vertex<>("H");
+        Edge AB = new Edge(A, B, 10);
+        Edge AC = new Edge(A, C, 50);
+        Edge BA = new Edge(B, A, 20);
+        Edge BH = new Edge(B, H, 40);
+        Edge CB = new Edge(C, B, 30);
+        Edge CD = new Edge(C, D, 40);
+        Edge DE = new Edge(D, E, 15);
+        Edge EF = new Edge(E, F, 25);
+        Edge FD = new Edge(F, D, 50);
+        Edge EG = new Edge(E, G, 50);
+        Edge GE = new Edge(G, E, 50);
+        A.addEdge(AB);
+        A.addEdge(AC);
+        B.addEdge(BA);
+        B.addEdge(BH);
+        C.addEdge(CB);
+        C.addEdge(CD);
+        D.addEdge(DE);
+        E.addEdge(EF);
+        E.addEdge(EG);
+        F.addEdge(FD);
+        G.addEdge(GE);
+
+        // Add an edge to the graph
+        System.out.println(A);
+        graph.addRoot(A);
+        graph.addRoot(L);
+    }
 }
--- Vertex.java
+++ Vertex.java
@@ -1,40 +1,137 @@
 import java.util.LinkedList;
 
+/**
+ * A vertex which is composed of N weighted edges and an optional value.
+ * @author Mourad, Mounir
+ * @param <T> Value type of the vertex
+ */
 public class Vertex<T> {
-  private T value;
-  private LinkedList<Edge> edges;
-  private boolean visited;
 
-  public Vertex() {
-    this(null, new LinkedList<>(), false);
-  }
+    /**
+     * Value of the vertex.
+     */
+    private T value;
 
-  public Vertex(T value) {
-    this(value, new LinkedList<>(), false);
-  }
+    /**
+     * List of edges that have this node for origin.
+     */
+    private LinkedList<Edge> edges;
 
-  public Vertex(T value, LinkedList<Edge> edges, boolean visited) {
-    this.value = value;
-    this.edges = edges;
-    this.visited = visited;
-  }
+    /**
+     * Indicates if this vertex is already visited or not.
+     * Must be set to false after every graph traversal.
+     */
+    private boolean visited;
 
-  public boolean addEdge(Edge e) {
-    return this.edges.add(e);
-  }
+    /**
+     * Constructs a vertex without edges nor value.
+     */
+    public Vertex() {
+        this(null, new LinkedList<>(), false);
+    }
 
-  public boolean removeEdge(Edge e) {
-    return this.edges.remove(e);
-  }
+    /**
+     * Constructs a new vertex with a value but no edges.
+     * @param value Value of the vertex.
+     */
+    public Vertex(T value) {
+        this(value, new LinkedList<>(), false);
+    }
 
-  // Getters
-  public T getValue() { return this.value; }
-  public LinkedList<Edge> getEdges() { return this.edges; }
-  public boolean isVisited() { return this.visited; }
+    /**
+     * Constructs a new vertex with a value, a set of edges and an initial value
+     * that indicates if this vertex has already been visited or not.
+     * @param value Value of the vertex.
+     * @param edges Set of edges connected to this vertex.
+     * @param visited Indicates if this vertex has already been visited or not.
+     */
+    public Vertex(T value, LinkedList<Edge> edges, boolean visited) {
+        this.value = value;
+        this.edges = edges;
+        this.visited = visited;
+    }
 
-  // Setters
-  public void setValue(T value) { this.value = value; }
-  public void setEdges(LinkedList<Edge> edges) { this.edges = edges; }
-  public void setVisited(boolean value) { this.visited = value; }
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public boolean addEdge(Edge e) {
+        return this.edges.add(e);
+    }
+
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public boolean removeEdge(Edge e) {
+        return this.edges.remove(e);
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        String s = "Vertex : " + this.value.toString() + "\n";
+        for(Edge e : this.edges)
+            s += " -> has an edge pointing to : " + e.printDestination() + ", and a weight of : " + e.getLabel() + "\n";
+        return s;
+    }
+
+    // ============== GETTERS ============== //
+    /**
+     *
+     * @return
+     */
+    public T getValue() {
+        return this.value;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public LinkedList<Edge> getEdges() {
+        return this.edges;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isVisited() {
+        return this.visited;
+    }
+
+
+    // ============== SETTERS ============== //
+
+
+    /**
+     *
+     * @param value
+     */
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    /**
+     *
+     * @param edges
+     */
+    public void setEdges(LinkedList<Edge> edges) {
+        this.edges = edges;
+    }
+
+    /**
+     *
+     * @param value
+     */
+    public void setVisited(boolean value) {
+        this.visited = value;
+    }
 
 }
